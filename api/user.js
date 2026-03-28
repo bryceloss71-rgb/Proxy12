1d9446d770b700f4ec5167b5eed99d64c8ec1f74
bryceloss71-rgb <bryceloss71@gmail.com>
2026-03-27 20:35:26 -0400

    w

--- api/user.js
+++ api/user.js
@@ -1,8 +1,13 @@
-// api/user.js
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-    res.status(200).json({ name: 'John Doe' });
-  } else {
-    res.status(405).send('Method Not Allowed');
-  }
-}
+const express = require('express');
+const { uvPath } = require('@titaniumnetwork-dev/ultraviolet');
+const app = express();
+
+// This tells Express to serve the actual UV engine files 
+// from the node_modules folder when /uv/ is requested.
+app.use('/uv/', express.static(uvPath));
+
+app.get('/api/status', (req, res) => {
+  res.json({ status: 'running' });
+});
+
+module.exports = app;
